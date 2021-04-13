9ba4999db3c379a678bd4b58d27dbff4409e3d37
yerimkoko <gochi97@naver.com>
2021-04-13 14:10:08 +0900

    210413 오늘의 문제

---
+++ venv/lib/python3.8/210413.py
@@ -0,0 +1,33 @@
+# 6026
+# a = input()
+# b = input()
+#
+# print(float(a)+float(b))
+
+# 6027
+# a = input()
+# a = int(a)
+# print('%x' %a)
+
+# 6028
+# a = input()
+# a = int(a)
+# print('%X' %a)
+
+# 6029
+# a = input()
+# n = int(a, 16)  # 16진수로 인식해서 n에 저장하기.
+# print('%o' %n)
+
+# 6030
+# c = str(input())
+# n = ord(c) # 영문자 하나를 입력받아 유니 코드 값으로 변환해주는 것.
+# print(n)
+
+# 6031
+# c = int(input())
+# print(chr(c)) # 정수를 유니코드로 변환해주는것.
+
+# 6032
+# a = int(input())
+# print(-a)

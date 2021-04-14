9756fc54d56e534e41b9813b1130595751ec9b96
yerimkoko <gochi97@naver.com>
2021-04-14 14:11:09 +0900

    210414 알고리즘

---
+++ venv/lib/python3.8/210414.py
@@ -0,0 +1,68 @@
+# 6033
+# a = input()
+# b = (ord(a)+1)
+# c = chr(b)
+# print(c)
+
+# 6034
+# a, b = input().split(' ')
+# c = int(a) - int(b)
+# print(c)
+
+# 6035
+# a, b = input().split(' ')
+# print(float(a) * float(b))
+
+# 6036
+# a, b = input().split(' ')
+# for i in range(int(b)):
+#     print(str(a))
+# 번외로!
+# s1, n = input().split(' ')
+# print(int(n) * str(s1))
+
+# 6037
+# n = input()
+# s1 = input()
+# print(int(n)*str(s1))
+
+# # 6038
+# a, b = input().split(' ')
+# print(int(a)**int(b))
+
+# # 6039
+# a, b = input().split(' ')
+# print(float(a)**float(b))
+
+# # 6040
+# a, b = input().split(' ')
+# print(int(a) // int(b))
+
+# 6041
+# a, b = input().split(' ')
+# print(int(a) % int(b))
+
+# 6042
+# a = float(input())
+# print(format(a, ".2f"))
+
+# # 6043
+# a, b = input().split(' ')
+# print(format(float(a)/float(b), ".3f"))
+
+# 6044
+n1, n2 = input().split()
+op1 = int(n1) + int(n2)
+op2 = int(n1) - int(n2)
+op3 = int(n1) * int(n2)
+op4 = int(n1) // int(n2)
+op5 = int(n1) % int(n2)
+op6 = format(float(n1) / float(n2), ".2")
+print(op1)
+print(op2)
+print(op3)
+print(op4)
+print(op5)
+print(op6)
+
+

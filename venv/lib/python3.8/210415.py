b8665d3aea099dae9421dcd32403b3ca00503828
yerimkoko <gochi97@naver.com>
2021-04-15 17:29:27 +0900

    210415 알고리즘

---
+++ venv/lib/python3.8/210415.py
@@ -0,0 +1,63 @@
+# # 6044
+# a, b = map(int, input().split())
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a // b)
+# print(a % b)
+# print(format(float(a / b), ".2f"))
+
+# 6045
+# a, b, c, = map(int, input().split(' '))
+# print(a+b+c, format(float((a+b+c)/3), ".2f"))
+
+# 6046
+# a = int(input())
+# print(a << 1)
+
+# 6047
+# a, b = map(int, input().split(' '))
+# print(a * 2 ** b)
+
+# 6048
+# a, b = map(int, input().split(' '))
+# if a < b:
+#     print(True)
+# else:
+#     print(False)
+
+# # 6049
+# a, b = map(int, input().split(' '))
+# if a == b:
+#     print(True)
+# else:
+#     print(False)
+
+# 6050
+# a, b = map(int, input().split(' '))
+# if a <= b:
+#     print(True)
+# else:
+#     print(False)
+
+# 6051
+# a, b = map(int, input().split(' '))
+# if a == b:
+#     print(False)
+# else:
+#     print(True)
+
+# 6052
+# n = int(input())
+# if n == 0:
+#     print(False)
+# else:
+#     print(bool(n))
+
+# 6053
+# n = bool(int(input()))
+# print(not n)
+
+# 6054
+n, m = map(int, input().split(' '))
+print(bool(n) and bool(m))

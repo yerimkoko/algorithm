29ed5ad5862123889d086b8878bcda7ddb24a469
yerimkoko <gochi97@naver.com>
2021-04-19 12:25:55 +0900

    210419

--- venv/lib/python3.8/210416.py
+++ venv/lib/python3.8/210416.py
@@ -36,6 +36,6 @@
 # print(c)
 
 # 6064
-a, b, c = map(int, input().split(' '))
-d = (a if a<b else b) if ((a if a<b else b) < c) else c
-print(d)
+# a, b, c = map(int, input().split(' '))
+# d = (a if a<b else b) if ((a if a<b else b) < c) else c
+# print(d)
---
+++ venv/lib/python3.8/210419.py
@@ -0,0 +1,94 @@
+# # 6065
+# a, b, c = map(int, input().split(' '))
+# if a % 2 == 0:
+#     print(a)
+# if b % 2 == 0:
+#     print(b)
+# if c % 2 == 0:
+#     print(c)
+
+# # 6066
+# a, b, c = map(int, input().split(' '))
+# arr = [a, b, c]
+# for i in arr:
+#     if i % 2 == 0:
+#         print("even")
+#     else:
+#         print("odd")
+
+# 6067
+# n = int(input())
+# if n < 0:
+#     if n % 2 == 0:
+#         print("A")
+#     else:
+#         print("B")
+# if n > 0:
+#     if n % 2 == 0:
+#         print("C")
+#     else:
+#         print("D")
+
+# 6068
+# n = int(input())
+# if 90 <= n <= 100:
+#     print("A")
+# elif 70 <= n < 90:
+#     print("B")
+# elif 40 <= n < 70:
+#     print("C")
+# else:
+#     print("D")
+
+
+# 6069
+# n = input()
+# if n == 'A':
+#     print("best!!!")
+# elif n == 'B':
+#     print("good!!")
+# elif n == 'C':
+#     print("run!")
+# elif n == 'D':
+#     print("slowly~")
+# else:
+#     print("what?")
+
+# # 6070
+# n = int(input())
+# if n // 3 == 1:
+#     print("spring")
+# elif n // 3 == 2:
+#     print("summer")
+# elif n // 3 == 3:
+#     print("fall")
+# elif (n // 3 == 0) or n == 12:
+#     print("winter)
+# else:
+#     print("다시 입력하세요")
+
+# 6071
+# n = 2
+# while int(n) != 0:
+#     n = int(input())
+#     if n != 0:
+#         print(n)
+
+# # 6072
+# n = int(input())
+# while n != 0:
+#     print(n)
+#     n -= 1
+
+# 6073
+# n = int(input())
+# while n != 0:
+#     n -= 1
+#     print(n)
+
+# 6074
+n = ord(input())
+t = ord('a')
+while t <= n:
+    print(chr(t), end=' ')
+    t += 1

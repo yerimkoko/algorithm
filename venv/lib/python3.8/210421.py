8659b20d6cbaade524f87b67dbc9b1825137ddf2
yerimkoko <gochi97@naver.com>
2021-04-21 15:10:13 +0900

    210421

---
+++ venv/lib/python3.8/210421.py
@@ -0,0 +1,35 @@
+# 6080
+# n, m = map(int, input().split())
+# for i in range(1, n+1):
+#   for j in range(1, m+1):
+#     print(i, j)
+
+# 6081
+# a = input()
+# n = int(a, 16)
+# for i in range(1, 16):
+#     print('%X' % n, '*%X' % i, '=%X' % (n * i), sep='')
+
+# # 6082
+# n = int(input())
+# for i in range(1, n+1):
+#     if i % 10 == 3 or i % 10 == 6 or i % 10 == 9:
+#         print('X')
+#     else:
+#         print(i)
+
+# 6083
+# r,g,b = map(int, input().split())
+# if 0 <= r and g and b <= 127:
+#     for i in range(r):
+#         for j in range(g):
+#             for k in range(b):
+#                 print(i, j, k)
+#     print(r*g*b)
+
+# 6084
+# h, b, c, s = input()
+# Mb = 1
+# Mb = 2^b / 2^20
+
+

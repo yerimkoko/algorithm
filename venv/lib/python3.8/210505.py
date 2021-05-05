e756c16fe402bd56917432786207312b33167e42
yerimkoko <gochi97@naver.com>
2021-05-05 14:09:33 +0900

    210505

---
+++ venv/lib/python3.8/210505.py
@@ -0,0 +1,36 @@
+# 6092
+# n = int(input())
+# a = input().split()
+#
+# for i in range(n):
+#     a[i] = int(a[i])
+# d = []
+# for i in range(24):
+#     d.append(0)
+#
+# for i in range(n):
+#     d[a[i]] += 1
+#
+# for i in range(1, 24):
+#     print(d[i], end=' ')
+
+# # 6093
+# n = int(input())
+# a = input().split()
+#
+# for i in range(n-1, -1, -1):
+#     print(a[i], end=' ')
+
+# # 6094
+# n = int(input())
+# k = map(int, input().split())
+# a = min(k)
+# print(a)
+
+# 6095
+# d = []
+# for i in range(20):
+#     d.append([])
+# for i in range(20):
+#     d.append(0)
+
